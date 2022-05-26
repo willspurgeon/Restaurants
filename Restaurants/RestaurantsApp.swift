2b8cf0cda76af187e13f14347130e76385cb059a
Will Spurgeon <will@spurgeonworld.com>
2022-05-26 16:37:26 -0400

    Initial Commit

---
+++ Restaurants/RestaurantsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestaurantsApp.swift
+//  Restaurants
+//
+//  Created by Will Spurgeon on 5/26/22.
+//
+
+import SwiftUI
+
+@main
+struct RestaurantsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
